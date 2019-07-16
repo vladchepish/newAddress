79949712a512382a91d83bf936dd6cce9a06e6f9
vladf <vladforwork@yandex.ru>
2019-07-16 21:22:21 +0300

    added GroupModificationTest + some corrections * added contacts class

--- Autotest/src/test/java/lib/Constants.java
+++ Autotest/src/test/java/lib/Constants.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Constants {
 
-    public static final List<String> ETALON_UPPER_MENU_ITEMS = Arrays.asList("home", "add new", "groups", "next birthdays", "print all",
-            "print phones", "map", "export", "import");
+    public static final List<String> ETALON_UPPER_MENU_ITEMS = Arrays.asList("home", "add new", "groups",
+            "next birthdays", "print all", "print phones", "map", "export", "import");
 
 }
