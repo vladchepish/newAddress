2aececde481466d3470e2241707e7eede7faf397
vladf <vladforwork@yandex.ru>
2019-07-26 11:02:38 +0300

    success dv connection

--- Autotest/src/test/java/tests/TestDbConnection.java
+++ Autotest/src/test/java/tests/TestDbConnection.java
@@ -1,10 +1,11 @@
 package tests;
 
+import objects.Group;
 import org.testng.annotations.Test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestDbConnection {
 
@@ -12,9 +13,8 @@ public class TestDbConnection {
     public void testConnection() {
         Connection conn = null;
         try {
-            conn = DriverManager.getConnection("jdbc:mysql://u0541324.cp.regruhosting.ru:2083/u0541324_addressbook?" +
-                    "user=u0541324_addressbook&password=LaubKupoWrabca7&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
-            /**
+            conn = DriverManager.getConnection("jdbc:mysql://31.31.196.140:3306/u0541324_addressbook?" +
+                    "user=u0541324_address&password=LaubKupoWrabca7");
              Statement st = conn.createStatement();
              ResultSet rs = st.executeQuery("select group_id, group_name, group_header, group_footer from group_list");
              List<Group> groups = new ArrayList<>();
@@ -28,7 +28,6 @@ public class TestDbConnection {
              st.close();
              conn.close();
              System.out.println(groups);
-             */
         } catch (SQLException ex) {
             System.out.println("SQLException: " + ex.getMessage());
             System.out.println("SQLState: " + ex.getSQLState());
