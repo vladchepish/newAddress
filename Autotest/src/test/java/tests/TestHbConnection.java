ea34946b4b96e74f5a39a31acca8d4899f824546
ф <>
2019-07-26 23:10:43 +0300

    added habirnate connection

---
+++ Autotest/src/test/java/tests/TestHbConnection.java
@@ -0,0 +1,47 @@
+package tests;
+
+import objects.Group;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class TestHbConnection {
+
+    private SessionFactory sessionFactory;
+
+    @BeforeClass
+    protected void setUp() throws Exception {
+        // A SessionFactory is set up once for an application!
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure() // configures settings from hibernate.cfg.xml
+                .build();
+        try {
+            sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
+            // so destroy it manually.
+            StandardServiceRegistryBuilder.destroy( registry );
+        }
+    }
+
+    @Test
+    public void test(){
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        List<Group> result = session.createQuery( "from Group" ).list();
+        for ( Group group : (List<Group>) result ) {
+            System.out.println(group);
+        }
+        session.getTransaction().commit();
+        session.close();
+    }
+
+}
